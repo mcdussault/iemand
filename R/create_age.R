2a10f365bee50adae9a7780ab3c3c73d8233bc6e
Dr Marie Christine Dussault <mcdussault@hotmail.co.uk>
2018-03-16 10:05:55 +0200

    Initial commit

---
+++ R/create_age.R
@@ -0,0 +1,10 @@
+#' Create an age for someone
+#' @details This is a useless function we created to learn! Gives an age
+#' @return age (Integer)
+#' @export
+#'
+#' @examples
+create_age <- function (){
+  age<- sample(1:100, size =1)
+  return(age)
+}
