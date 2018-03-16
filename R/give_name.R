ef79150780ca448894a55d60648671a8efb98c40
Dr Marie Christine Dussault <mcdussault@hotmail.co.uk>
2018-03-16 13:03:59 +0200

    give_name documentation

--- R/give_name.R
+++ R/give_name.R
@@ -1,3 +1,11 @@
+#' Gives a name based on assigned gender
+#'
+#' @param sex
+#'
+#' @return Returns a name
+#' @export
+#'
+#' @examples
 give_name<- function(sex = "female"){
   possible_names<- sa_names$first_name[sa_names$sex == sex]
   sample(possible_names, size = 1)
